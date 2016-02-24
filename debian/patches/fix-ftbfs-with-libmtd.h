872ee76f82d7c8a4630ac4a4512263d38faebdd7
Martin Steigerwald <martin.steigerwald@teamix.de>
2016-02-24 11:17:58 +0100

    Patch works, so describe it and add a changelog entry.

--- debian/patches/fix-ftbfs-with-libmtd.h
+++ debian/patches/fix-ftbfs-with-libmtd.h
@@ -1,3 +1,6 @@
+Description: fix FTBFS libmtd.h:288:8: error: unknown type name 'uint8_t' (Debian Bug 815735)
+Author: Martin Steigerwald <martin.steigerwald@teamix.de>
+
 --- a/oslib/libmtd.h
 +++ b/oslib/libmtd.h
 @@ -29,6 +29,8 @@
